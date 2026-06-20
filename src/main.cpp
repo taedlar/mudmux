99d7ed08794713bed87de7a86fb448c42a825463
Ted Chang <taedlar@gmail.com>
2026-06-20 21:47:49 +0800

    feat: init cmake settings

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+// main.cpp
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <iostream>
+#include <spdlog/spdlog.h>
+
+int main() {
+    spdlog::info("Hello, MudMux!");
+    return 0;
+}
