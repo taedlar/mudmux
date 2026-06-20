a25b71c4b4ce4a8b09500c5bd045e286fd926dde
Ted Chang <taedlar@gmail.com>
2026-06-21 00:48:35 +0800

    feat: add dependencies

---
+++ cmake/setup.cmake
@@ -0,0 +1,27 @@
+# cmake/setup.cmake
+
+include(FetchContent)
+
+FetchContent_Declare(
+    GTest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG v1.17.0
+    FIND_PACKAGE_ARGS
+        NAMES GTest
+)
+
+FetchContent_Declare(
+    OpenSSL
+    GIT_REPOSITORY https://github.com/openssl/openssl.git
+    GIT_TAG OpenSSL_1_1_1-stable
+    FIND_PACKAGE_ARGS
+        NAMES OpenSSL
+)
+
+FetchContent_Declare(
+    spdlog
+    GIT_REPOSITORY https://github.com/gabime/spdlog.git
+    GIT_TAG v1.9.2
+    FIND_PACKAGE_ARGS
+        NAMES spdlog
+)
