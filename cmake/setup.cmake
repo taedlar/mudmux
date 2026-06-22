890b0eff6c1b6c0a80c2206ccb0467840d8cf299
Ted Chang <taedlar@gmail.com>
2026-06-23 02:02:50 +0800

    feat: detect boost with config mode

--- cmake/setup.cmake
+++ cmake/setup.cmake
@@ -27,12 +27,24 @@ FetchContent_Declare(
 )
 
 # Optional Boost dependency
-cmake_policy (SET CMP0167 OLD) # Allow FindBoost to use BOOST_ROOT
+set(Boost_DEBUG ON)
 if (BOOST_ROOT)
     # If BOOST_ROOT is set, we assume the user has Boost installed and wants to use it.
+    # In some cases, when the FindBoost module fails to find the Boost libraries, it
+    # may be necessary to set BOOST_ROOT to the root directory of the Boost installation.
+    if (POLICY CMP0167)
+        cmake_policy(SET CMP0167 OLD) # Use old behavior for FindBoost
+    endif()
     find_package(Boost REQUIRED)
 else()
-    find_package(Boost QUIET) # FindBoost.cmake or library lookup fallback
+    # Modern CMake removes the FindBoost.cmake module and rely on Boost's config files.
+    # In this case, we'll need to supply hints for popular Linux distributions.
+    find_package(Boost CONFIG HINTS
+        /usr/lib/x86_64-linux-gnu/cmake # Debian/Ubuntu
+        /usr/lib64/cmake # Fedora/Red Hat
+        /usr/local/lib/cmake # Custom installations
+        /opt/local/lib/cmake # MacPorts
+    )
 endif()
 if (Boost_FOUND)
     set(HAVE_BOOST TRUE)
