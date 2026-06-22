68c4272279b0a5f9151a4dcf8ea66cbc936e9fc8
Ted Chang <taedlar@gmail.com>
2026-06-23 00:18:23 +0800

    feat: boost detection (#7)

--- cmake/setup.cmake
+++ cmake/setup.cmake
@@ -15,7 +15,7 @@ FetchContent_Declare(
     GIT_REPOSITORY https://github.com/openssl/openssl.git
     GIT_TAG openssl-3.6.3
     FIND_PACKAGE_ARGS
-        NAMES OpenSSL
+        NAMES OpenSSL openssl
 )
 
 FetchContent_Declare(
@@ -24,4 +24,30 @@ FetchContent_Declare(
     GIT_TAG v1.9.2
     FIND_PACKAGE_ARGS
         NAMES spdlog
+)
+
+# Optional Boost dependency
+cmake_policy (SET CMP0167 OLD) # Allow FindBoost to use BOOST_ROOT
+if (BOOST_ROOT)
+    # If BOOST_ROOT is set, we assume the user has Boost installed and wants to use it.
+    find_package(Boost REQUIRED)
+else()
+    find_package(Boost QUIET) # FindBoost.cmake or library lookup fallback
+endif()
+if (Boost_FOUND)
+    set(HAVE_BOOST TRUE)
+endif()
+
+# =========================
+# CMake Dependency Provider
+# =========================
+
+macro(setup_provide_dependency method package)
+    message(STATUS "Providing dependency '${package}'")
+endmacro()
+
+cmake_language(
+    SET_DEPENDENCY_PROVIDER setup_provide_dependency
+    SUPPORTED_METHODS
+        FETCHCONTENT_MAKEAVAILABLE_SERIAL
 )
