ce53c1691c67ad7f48b55044e9fe1af0d546f11d
Ted Chang <taedlar@gmail.com>
2026-07-08 21:37:48 +0800

    test: add unit tests for mudmux APIs

---
+++ tests/unit_mudmux/test_mudmux.cpp
@@ -0,0 +1,35 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <gtest/gtest.h>
+#include <thread>
+
+#include "mudmux/mudmux.h"
+
+TEST(MudmuxTest, BasicInitialization) {
+    // Test that the mudmux library initializes correctly
+    ASSERT_TRUE(mudmux_init(nullptr));
+    ASSERT_NO_FATAL_FAILURE(mudmux_deinit());
+}
+
+TEST(MudmuxTest, EventLoopRun) {
+    // Test that the mudmux event loop can run and shutdown correctly
+    ASSERT_TRUE(mudmux_init(nullptr));
+    
+    std::thread server_thread([]() {
+        int result = mudmux_run(nullptr);
+        ASSERT_EQ(result, EXIT_SUCCESS);
+    });
+
+    // Allow some time for the server to start
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    // Shutdown the server
+    mudmux_shutdown();
+
+    // Wait for the server thread to finish
+    server_thread.join();
+
+    ASSERT_NO_FATAL_FAILURE(mudmux_deinit());
+}
