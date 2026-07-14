0d475987aabea834658d19c511cb97a00d677843
Ted Chang <taedlar@gmail.com>
2026-07-14 13:10:35 +0800

    feat: add logon in example chatroom

--- examples/chatroom/user.cpp
+++ examples/chatroom/user.cpp
@@ -7,6 +7,8 @@
 #include "mudmux/comm.h"
 #include "user.hpp"
 
+std::vector<std::shared_ptr<User>> User::slots; // mapping of comm slots to User instances
+
 void User::logon() {
     comm_abstract_t* comm = comm_abstract_get(comm_slot);
     if (!comm)
@@ -25,10 +27,14 @@ void User::disconnect() {
 }
 
 void User::prompt() {
+    if (inbound_handler) {
+        // If an inbound handler is set, we don't display the prompt to avoid confusion
+        return;
+    }
     comm_abstract_t* comm = comm_abstract_get(comm_slot);
     if (!comm)
         return;
-    *comm << "> "; // display prompt for user input
+    *comm << "[" << username << "] "; // display prompt for user input
 }
 
 void User::dispatchInboundMessage(const std::string& message) {
@@ -36,10 +42,32 @@ void User::dispatchInboundMessage(const std::string& message) {
         (this->*inbound_handler)(message); // call the inbound handler function
     } else {
         auto comm = comm_abstract_get(comm_slot);
-        if (comm)
-            *comm << fmt::format("Received message: [{}]\n\r", message);
-        if (message == "quit" || message == "exit") {
-            comm_close(nullptr, comm_slot); // close the connection on "quit" or "exit"
+        if (!comm)
+            return;
+        if (message.front() == '/') {
+            // Handle slash commands
+            if (message == "/help") {
+                *comm << "Available commands:\n\r";
+                *comm << "/help - Show this help message\n\r";
+                *comm << "/quit - Disconnect from the chatroom\n\r";
+                *comm << "/exit - Disconnect from the chatroom\n\r";
+            } else if (message == "/quit" || message == "/exit") {
+                comm_close(nullptr, comm_slot); // close the connection on "quit" or "exit"
+            } else {
+                *comm << "Unknown command: " << message << "\n\r";
+            }
+        } else {
+            // Handle regular chat messages
+            *comm << "You said: " << message << "\n\r"; // echo the message back to the user
+            // Broadcast the message to all other connected users
+            for (const auto& user_ptr : User::slots) {
+                if (user_ptr && user_ptr->getState() == UserState::LoggedIn && user_ptr->comm_slot != comm_slot) {
+                    auto other_comm = user_ptr->getComm();
+                    if (other_comm) {
+                        *other_comm << username << " says: " << message << "\n\r"; // broadcast the message to other users
+                    }
+                }
+            }
         }
     }
 }
@@ -49,6 +77,9 @@ void User::receiveUsername(const std::string& name) {
     state = UserState::LoggedIn; // set state to LoggedIn after receiving the username
     inbound_handler = nullptr; // reset the inbound handler to nullptr since we no longer need it
     auto comm = comm_abstract_get(comm_slot);
-    if (comm)
+    if (comm) {
         *comm << fmt::format("Welcome, {}!\n\r", username);
+        *comm << "You are now logged in. Type your messages to chat with others.\n\r";
+        *comm << "You can also use slash commands like /help, /quit, etc. to interact with the chatroom.\n\r";
+    }
 }
