15a187676fec90bbbbe1e7eae6e78d38608a0073
Ted Chang <taedlar@gmail.com>
2026-08-20 22:07:46 +0800

    fix: dependency override find_package (#119)

--- cmake/fetch-settings.cmake
+++ cmake/fetch-settings.cmake
@@ -6,6 +6,7 @@ FetchContent_Declare(
     GTest
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_TAG v1.17.0
+    OVERRIDE_FIND_PACKAGE
 )
 set(INSTALL_GTEST OFF CACHE BOOL "Disable installation of googletest" FORCE) # we don't want to install GTest, just use it for testing
 
@@ -13,16 +14,14 @@ FetchContent_Declare(
     OpenSSL
     GIT_REPOSITORY https://github.com/openssl/openssl.git
     GIT_TAG openssl-3.6.3
-    FIND_PACKAGE_ARGS
-        NAMES OpenSSL openssl
+    OVERRIDE_FIND_PACKAGE
 )
 
 FetchContent_Declare(
     spdlog
     GIT_REPOSITORY https://github.com/gabime/spdlog.git
     GIT_TAG v1.17.0
-    FIND_PACKAGE_ARGS
-        NAMES spdlog
+    OVERRIDE_FIND_PACKAGE
 )
 set(SPDLOG_PREVENT_CHILD_FD ON CACHE BOOL "Prevent child process inherit spdlog file descriptors" FORCE) # for security
 
@@ -30,8 +29,7 @@ FetchContent_Declare(
     argparse
     GIT_REPOSITORY https://github.com/p-ranav/argparse.git
     GIT_TAG v3.2
-    FIND_PACKAGE_ARGS
-        NAMES argparse
+    OVERRIDE_FIND_PACKAGE
 )
 
 # FIXME: Promote this to a proper release tag once the issues of VS2019 builds 
@@ -40,8 +38,7 @@ FetchContent_Declare(
     yaml-cpp
     GIT_REPOSITORY https://github.com/jbeder/yaml-cpp.git
     GIT_TAG fb9107556b1e28028091f39d5d7d0c9c8758cfbd
-    FIND_PACKAGE_ARGS
-        NAMES yaml-cpp
+    OVERRIDE_FIND_PACKAGE
 )
 set(YAML_CPP_BUILD_CONTRIB OFF CACHE BOOL "" FORCE)
 set(YAML_CPP_BUILD_TOOLS OFF CACHE BOOL "" FORCE)
